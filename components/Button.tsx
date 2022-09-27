d372b489d2caaef701c4dd7051c2bbd06aadf6b2
zxzx93 <thddustn26@hanmail.net>
2022-09-28 01:19:27 +0900

    [Design] 버튼 컴포넌트 css 변경

--- components/Button.tsx
+++ components/Button.tsx
@@ -7,22 +7,30 @@ interface Props {
 	loading?: boolean;
 	padding?: string;
 	noIcon?: boolean;
+	buttonType?: 'cart';
 }
 
-function Button({ title, onClick, width, loading, padding, noIcon }: Props) {
+function Button({
+	title,
+	onClick,
+	width,
+	loading,
+	padding,
+	noIcon,
+	buttonType,
+}: Props) {
 	return (
 		<button
-			className={`ease group relative z-30 box-border inline-flex ${
+			className={`relative box-border inline-flex cursor-pointer items-center justify-center overflow-hidden rounded-full border border-[#000] text-sm  ${
 				width || 'w-auto'
-			} ${padding} cursor-pointer items-center justify-center overflow-hidden rounded bg-[#35383C] px-8 py-3 font-bold text-white transition-all duration-300 focus:outline-none`}
+			} ${padding} ${
+				buttonType === 'cart' ? 'bg-black text-white' : 'bg-white text-black'
+			}`}
 			onClick={onClick}
 			type='button'
 		>
-			<span className='absolute bottom-0 right-0 -mb-8 -mr-5 h-20 w-8 translate-x-1 rotate-45 transform bg-white opacity-10 transition-all duration-300 ease-out group-hover:translate-x-0' />
-			<span className='absolute top-0 left-0 -mt-1 -ml-12 h-8 w-20 -translate-x-1 -rotate-45 transform bg-white opacity-10 transition-all duration-300 ease-out group-hover:translate-x-0' />
-
-			<span className='relative z-20 flex items-center font-semibold'>
-				{noIcon && (
+			<span className='relative  flex h-[56px] items-center font-medium '>
+				{/* {noIcon && (
 					<svg
 						className='relative mr-2 h-5 w-5 flex-shrink-0 text-white'
 						fill='none'
@@ -37,7 +45,7 @@ function Button({ title, onClick, width, loading, padding, noIcon }: Props) {
 							d='M13 10V3L4 14h7v7l9-11h-7z'
 						/>
 					</svg>
-				)}
+				)} */}
 
 				{loading ? 'Loading...' : title}
 			</span>
