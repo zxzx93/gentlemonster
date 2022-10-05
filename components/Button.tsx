f2dbfc985caae6574da080cc61e3fa91fb1971fe
zxzx93 <thddustn26@hanmail.net>
2022-10-06 01:36:42 +0900

    [Add] 공통으로 사용하는 버튼 수정
    - 버튼 컬러 Props로 받을 수 있도록 수정
    - height도 Props로 받을 수 있도록 수정
    - 로딩 progress 추가

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,32 +4,38 @@ interface Props {
 	title: string;
 	onClick?: () => void;
 	width?: string;
+	height?: string;
 	loading?: boolean;
 	padding?: string;
 	noIcon?: boolean;
-	buttonType?: 'cart';
+	buttonColor?: 'black' | 'white';
 }
 
 function Button({
 	title,
 	onClick,
 	width,
+	height,
 	loading,
 	padding,
 	noIcon,
-	buttonType,
+	buttonColor,
 }: Props) {
 	return (
 		<button
-			className={`relative box-border inline-flex cursor-pointer items-center justify-center overflow-hidden rounded-full border border-[#000] text-sm  ${
+			className={`relative box-border inline-flex cursor-pointer items-center justify-center overflow-hidden border border-[#000] text-sm  ${
 				width || 'w-auto'
 			} ${padding} ${
-				buttonType === 'cart' ? 'bg-black text-white' : 'bg-white text-black'
+				buttonColor === 'black' ? 'bg-black text-white' : 'bg-white text-black'
 			}`}
 			onClick={onClick}
 			type='button'
 		>
-			<span className='relative  flex h-[56px] items-center font-medium '>
+			<span
+				className={`relative flex ${
+					height || 'h-[56px]'
+				} items-center font-medium`}
+			>
 				{/* {noIcon && (
 					<svg
 						className='relative mr-2 h-5 w-5 flex-shrink-0 text-white'
@@ -47,7 +53,30 @@ function Button({
 					</svg>
 				)} */}
 
-				{loading ? 'Loading...' : title}
+				{loading ? (
+					<svg
+						className='mr-3 h-5 w-5 animate-spin text-white'
+						xmlns='http://www.w3.org/2000/svg'
+						fill='none'
+						viewBox='0 0 24 24'
+					>
+						<circle
+							className='opacity-25'
+							cx='12'
+							cy='12'
+							r='10'
+							stroke='currentColor'
+							strokeWidth='4'
+						/>
+						<path
+							className='opacity-75'
+							fill='currentColor'
+							d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
+						/>
+					</svg>
+				) : (
+					title
+				)}
 			</span>
 		</button>
 	);
