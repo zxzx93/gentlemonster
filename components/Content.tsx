69db053dd2cebc2aca4a2dd5ea25de85535e5203
zxzx93 <thddustn26@hanmail.net>
2022-10-08 21:26:05 +0900

    [Feat] 상품 갯수 증가, 감소하는  버튼 추가하고 전체삭제 comfirm 추가시킴

--- components/Content.tsx
+++ components/Content.tsx
@@ -1,42 +1,110 @@
-import React from 'react';
 import Image from 'next/image';
-import Link from 'next/link';
+import React from 'react';
+import CurrencyFormat from 'react-currency-format';
+import toast from 'react-hot-toast';
 
-import { BsHeart, BsHeartFill } from 'react-icons/bs';
+import { useDispatch } from 'react-redux';
 import { Product } from '../typings';
 import { urlFor } from '../sanity';
+import { incQty, decQty, removeAllToCart } from '../redux/cartSlice';
 
 interface Props {
-	product: Product;
+	id: string;
+	contents: Product[];
+	location: 'cartModal' | 'cart';
 }
 
-function Content({ product }: Props) {
-	const click = false;
-	const HeartIcon = click ? BsHeartFill : BsHeart;
+type QtyProps = 'decQty' | 'incQty';
+
+function Content({ id, contents, location }: Props) {
+	const dispatch = useDispatch();
+
+	const removeAllContentFromCart = () => {
+		const removeConfirm = window.confirm('해당 상품을 삭제하시겠습니까?');
+
+		if (removeConfirm) {
+			dispatch(removeAllToCart({ id }));
+
+			toast.error(`${contents[0].title}를 삭제했습니다.`, {
+				position: 'bottom-center',
+			});
+		}
+	};
+
+	const itemQuantity = (qty: QtyProps) => {
+		if (qty === 'incQty') {
+			dispatch(incQty(contents[0]));
+		} else {
+			dispatch(decQty({ id }));
+		}
+	};
 
 	return (
-		<div className='flex h-fit w-[280px] select-none flex-col space-y-3 rounded-xl p-8 md:h-full md:w-[300px] md:p-8'>
-			<Link href={`/detail/${product._id}`}>
-				<div className='relative h-64 w-full cursor-pointer md:h-72'>
-					<Image
-						src={urlFor(product.image[0]).url()}
-						objectFit='contain'
-						layout='fill'
-					/>
-				</div>
-			</Link>
+		<div className='flex flex-row  border-b py-3.5 lg:flex-row '>
+			<div
+				className={`relative ${
+					location === 'cartModal' ? 'h-24 w-24' : 'h-44 w-44'
+				}`}
+			>
+				<Image
+					src={urlFor(contents[0].image[0]).url()}
+					layout='fill'
+					objectFit='contain'
+				/>
+			</div>
 
-			<div className='flex flex-1 justify-between px-3'>
-				<Link href={`/detail/${product._id}`}>
-					<div className='cursor-pointer space-y-1 text-sm text-black md:text-base'>
-						<p className='font-normal'>{product.title}</p>
-						<p className='text-[0.9em] font-light'>{product.price}원</p>
+			<div className='flex flex-1 items-start'>
+				<div className='relative h-full flex-1 space-y-4'>
+					<div className='justify-items  flex h-full flex-col items-start gap-x-8 text-sm'>
+						<h4 className='font-semibold'>{contents[0].title}</h4>
+						<p className='py-1'>
+							<CurrencyFormat
+								value={contents[0].price}
+								displayType='text'
+								thousandSeparator
+								suffix='원'
+							/>
+						</p>
+						<div className='pt-21 absolute bottom-0 flex gap-x-1 font-title'>
+							<div className='flex w-[80px] items-center justify-between border-[0.5px] border-zinc-600/75 px-2 py-1 text-center text-black'>
+								<button
+									type='button'
+									className='quantity'
+									onClick={() => itemQuantity('decQty')}
+									disabled={contents.length === 1}
+								>
+									-
+								</button>
+								<span className='quantity'>{contents.length}</span>
+								<button
+									type='button'
+									className='quantity'
+									onClick={() => itemQuantity('incQty')}
+								>
+									+
+								</button>
+							</div>
+						</div>
 					</div>
-				</Link>
+				</div>
 
-				<div className='flex cursor-pointer'>
-					{/* <BsCart3 className='h-4.5 w-4.5 text-black' /> */}
-					<HeartIcon className={`h-4.5 w-4.5 ${click && 'fill-red-500'}`} />
+				<div className='flex h-full flex-col justify-between text-sm'>
+					<h4 className='fonts-semiblod'>
+						<CurrencyFormat
+							value={contents.reduce((total, item) => total + item.price, 0)}
+							displayType='text'
+							thousandSeparator
+							suffix='원'
+						/>
+					</h4>
+
+					<button
+						type='button'
+						onClick={removeAllContentFromCart}
+						className='box-border self-end border-b border-black text-black'
+					>
+						삭제
+					</button>
 				</div>
 			</div>
 		</div>
