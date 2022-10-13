c248ca8c80dcffcac352d80b8512c7f3aeae0e79
zxzx93 <thddustn26@hanmail.net>
2022-10-13 23:15:34 +0900

    [Feat] 결제 성공 페이지 레이아웃 완료. 결제 상품에 대한 데이터 가져와서 보여줌
    -결제 상품들에 대한 이름, 수량 , 금액 등 가져와서 보여줌
    -주문자, 번호 ,배송 주소 등 배송 관련 정보 가져와서 보여줌

--- pages/success/index.tsx
+++ pages/success/index.tsx
@@ -11,14 +11,23 @@ import { GetServerSideProps } from 'next';
 
 import Button from '../../components/Button';
 import { StripeProducts } from '../../typings';
+import fetchLineItems from '../../utils/fetchLineItems';
 
 interface Props {
-	products: StripeProducts[];
+	products: StripeProducts;
 }
 
 function Success({ products }: Props) {
 	const router = useRouter();
-	const { session_id } = router.query;
+	const sessionId = router.query.session_id;
+	const { customer_details: orderAddress, line_items: orderItems } = products;
+	const {
+		state,
+		city,
+		line1,
+		line2,
+		postal_code: postalCode,
+	} = orderAddress.address;
 
 	const [mounted, setMounted] = useState(false);
 	useEffect(() => {
@@ -27,7 +36,7 @@ function Success({ products }: Props) {
 
 	const [showOrderSummary, setShowOrderSummary] = useState(false);
 
-	const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' });
+	const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1024px)' });
 	const showOrderSummaryCondition = isTabletOrMobile ? showOrderSummary : true;
 
 	const handleShowOrderSummary = () => {
@@ -37,6 +46,11 @@ function Success({ products }: Props) {
 	const ShowOrderSummaryIcon = showOrderSummaryCondition
 		? HiChevronUp
 		: HiChevronDown;
+
+	const subTotal = orderItems.reduce(
+		(acc, product) => acc + product.price.unit_amount,
+		0,
+	);
 
 	return (
 		<div>
@@ -60,7 +74,7 @@ function Success({ products }: Props) {
 			<main className='grid grid-cols-1 font-title lg:grid-cols-9'>
 				<section className='order-2 mx-auto max-w-xl pb-12 lg:col-span-5 lg:mx-0 lg:max-w-none lg:pr-16 lg:pt-16 xl:pl-16 2xl:pl-44'>
 					<Link href='/'>
-						<div className='relative ml-14 hidden h-24 w-12 cursor-pointer transition lg:inline-flex'>
+						<div className='relative ml-14 hidden  h-16 w-48 cursor-pointer transition lg:inline-flex'>
 							<Image
 								src='https://images.surfacemag.com/app/uploads/2017/06/gentlemonsterlogo-1024x83.png'
 								objectFit='contain'
@@ -69,51 +83,55 @@ function Success({ products }: Props) {
 						</div>
 					</Link>
 
-					<div className='my-8 ml-4 flex space-x-4 lg:ml-14 xl:ml-0'>
+					<div className='my-8 ml-4 flex space-x-4 lg:ml-14'>
 						<div className='flex h-11 w-11 items-center justify-center rounded-full  border-black'>
 							<BsCheckCircle className='h-8 w-8' />
 						</div>
 						<div>
-							<p className='text-sm text-gray-500'>
-								Order #{session_id?.slice(-5)}
+							<p className='text-xs text-gray-500'>
+								구매번호 #{sessionId?.slice(-5)}
 							</p>
-							<h4 className='text-lg'>
-								Thank you{' '}
+							<h4 className='text-base font-medium'>
+								주문이 완료 되었습니다.{' '}
 								{/* {session ? session.user?.name?.split(' ')[0] : 'Guest'} */}
 							</h4>
 						</div>
 					</div>
 
-					<div className='mx-4 divide-y divide-gray-300 rounded-md border border-gray-300 p-4 lg:ml-14'>
-						<div className='space-y-2 pb-3'>
-							<p>Your order is confirmed</p>
-							<p className='text-sm text-gray-600'>
-								We’ve accepted your order, and we’re getting it ready. Come back
-								to this page for updates on your shipment status.
-							</p>
-						</div>
-						<div className='pt-3 text-sm'>
-							<p className='font-medium text-gray-600'>
-								Other tracking number:
-							</p>
-							<p>CNB21441622</p>
-						</div>
-					</div>
+					<table className='mx-4 mb-4  flex table-fixed border-separate border-spacing-4 flex-col divide-y divide-gray-300 rounded-md border border-gray-300 p-4 lg:ml-14'>
+						<thead className='text-sm font-medium'>
+							<tr>
+								<th>배송지 정보</th>
+							</tr>
+						</thead>
 
-					<div className='my-4 mx-4 space-y-2 rounded-md border border-gray-300 p-4 lg:ml-14'>
-						<p>Order updates</p>
-						<p className='text-sm text-gray-600'>
-							You’ll get shipping and delivery updates by email and text.
-						</p>
-					</div>
-					<div className='mx-4 flex flex-col items-center justify-between text-sm lg:ml-14 lg:flex-row'>
-						<p className='hidden lg:inline'>Need help? Contact us</p>
+						<tbody className='text-sm'>
+							<tr>
+								<td className='pr-4'>받는사람 이름</td>
+								<td>{orderAddress.name}</td>
+							</tr>
+							<tr>
+								<td>연락처</td>
+								<td>{orderAddress.phone}</td>
+							</tr>
+							<tr>
+								<td>주소</td>
+								<td>
+									[{postalCode}] {`${state} ${city} ${line1} ${line2 ? line2 : ""}`}
+								</td>
+							</tr>
+						</tbody>
+					</table>
+
+					<div className='mx-4 flex flex-col items-center justify-end text-sm lg:ml-14 lg:flex-row'>
 						{mounted && (
 							<Button
 								title='쇼핑 계속하기'
 								onClick={() => router.push('/')}
 								width={isTabletOrMobile ? 'w-full' : undefined}
 								buttonColor='black'
+								padding='p-3'
+								height='inherit'
 							/>
 						)}
 					</div>
@@ -127,14 +145,18 @@ function Success({ products }: Props) {
 							} border-gray-300 text-sm lg:hidden`}
 						>
 							<div className='mx-auto flex max-w-xl items-center justify-between px-4 py-6'>
-								<button type='button' onClick={handleShowOrderSummary}>
+								<button
+									type='button'
+									onClick={handleShowOrderSummary}
+									className='flex items-center space-x-2'
+								>
 									<p>주문 상세내역</p>
 									<ShowOrderSummaryIcon className='text-black-500 h-4 w-4' />
 								</button>
 
 								<p className='text-xl font-medium'>
 									<CurrencyFormat
-										value={123}
+										value={subTotal}
 										displayType='text'
 										thousandSeparator
 										suffix='원'
@@ -142,6 +164,75 @@ function Success({ products }: Props) {
 								</p>
 							</div>
 						</div>
+
+						{showOrderSummaryCondition && (
+							<div className='mx-auto max-w-xl divide-y border-gray-300 px-4 py-4 lg:mx-0 lg:max-w-lg lg:px-10 lg:py-16'>
+								<div className='space-y-4 pb-4'>
+									{orderItems.map(product => (
+										<div
+											key={product.id}
+											className='flex items-center space-x-4 text-sm font-medium'
+										>
+											<div className='relative flex h-16 w-16 items-center justify-center rounded-md border border-gray-300 bg-[#F1F1F1] text-xs text-white'>
+												<div className='relative h-7 w-7 animate-bounce rounded-md'>
+													<Image
+														src='https://images.surfacemag.com/app/uploads/2017/06/gentlemonsterlogo-1024x83.png'
+														layout='fill'
+														objectFit='contain'
+													/>
+												</div>
+												<div className='absolute -right-2 -top-2 flex h-5 w-5 items-center justify-center rounded-full bg-[gray] text-xs'>
+													{product.quantity}
+												</div>
+											</div>
+
+											<p className='flex-1'>{product.description}</p>
+											<p>
+												<CurrencyFormat
+													value={product.price.unit_amount}
+													displayType='text'
+													thousandSeparator
+													suffix='원'
+												/>
+											</p>
+										</div>
+									))}
+								</div>
+
+								<div className='space-y-1 py-4'>
+									<div className='flex justify-between text-sm'>
+										<p className='text-[gray]'>소계</p>
+										<p className='font-medium'>
+											<CurrencyFormat
+												value={subTotal}
+												displayType='text'
+												thousandSeparator
+												suffix='원'
+											/>
+										</p>
+									</div>
+
+									<div className='flex justify-between text-sm'>
+										<p className='text-[gray]'>배송비</p>
+										<p className='text-[gray]'>무료</p>
+									</div>
+
+									<div className='flex justify-between pt-4'>
+										<p>결제 금액</p>
+										<p className='flex items-center gap-x-2 text-xs text-[gray]'>
+											<span className='text-xl font-medium text-black'>
+												<CurrencyFormat
+													value={subTotal}
+													displayType='text'
+													thousandSeparator
+													suffix='원'
+												/>
+											</span>
+										</p>
+									</div>
+								</div>
+							</div>
+						)}
 					</section>
 				)}
 			</main>
@@ -149,15 +240,13 @@ function Success({ products }: Props) {
 	);
 }
 
-export const getServerSideProps: GetServerSideProps<Props> = async ({
-	query,
-}) => {
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
 	const sessionId = query.session_id as string;
-	// const products = await fetchLineItems(sessionId);
+	const products = await fetchLineItems(sessionId); // 결제한 아이템들 정보 가져옴
 
 	return {
 		props: {
-			// products,
+			products,
 		},
 	};
 };
