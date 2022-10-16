b2d0b18ab4906de92024e264e68991adcf21bb94
zxzx93 <thddustn26@hanmail.net>
2022-10-16 23:33:52 +0900

    [Feat] 카트와 관심상품으로 담긴 상품들 localstorage 저장
    - redux-persist 이용해서 localstorage 저장해서 데이터가 비워지지 않게 처리함

--- redux/store.ts
+++ redux/store.ts
@@ -1,10 +1,44 @@
 import { configureStore } from '@reduxjs/toolkit';
+import {
+	persistReducer,
+	FLUSH,
+	REHYDRATE,
+	PAUSE,
+	PERSIST,
+	PURGE,
+	REGISTER,
+} from 'redux-persist';
+import { combineReducers } from 'redux';
+import storage from 'redux-persist/lib/storage';
+// SessionStorage 사용의 경우 import storageSession from 'redux-persist/lib/storage/session
 import cartReducer from './cartSlice';
+import wishlistReducer from './wishlistSlice';
+
+const persistConfig = {
+	key: 'root',
+	version: 1,
+	storage,
+	// if you do not want to persist this part of the state
+	// blacklist: ['wishlist'],
+};
+
+const reducer = combineReducers({
+	cart: cartReducer,
+	wishlist: wishlistReducer,
+	// not persisting this reducer
+	// omitedPart: OmitReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducer);
 
 export const store = configureStore({
-	reducer: {
-		cart: cartReducer,
-	},
+	reducer: persistedReducer,
+	middleware: getDefaultMiddleware =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+			},
+		}),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
