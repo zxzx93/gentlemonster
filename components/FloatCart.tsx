5547035c97d71d19228160b4e2e3f6f2046c6552
zxzx93 <thddustn26@hanmail.net>
2022-10-19 23:23:38 +0900

    [Add] 메인 페이지 오른쪽 하단의 카트에 담긴 상품 수량 변경
    - 수량확인 : hooks으로 만든것으로 변경

--- components/FloatCart.tsx
+++ components/FloatCart.tsx
@@ -2,18 +2,20 @@ import Link from 'next/link';
 import { useSelector } from 'react-redux';
 import { HiOutlineShoppingBag } from 'react-icons/hi';
 import { selectCartItems } from '../redux/cartSlice';
+import useQuantity from '../hooks/useQuantity';
 
 function FloatCart() {
 	const items = useSelector(selectCartItems);
+	const { cartQuantity } = useQuantity();
 
-  if (items.length === 0) return null;
+	if (cartQuantity === 0) return null;
 
 	return (
 		<Link href='/cart'>
 			<div className='fixed bottom-10 right-10 z-50 flex h-16 w-16 cursor-pointer items-center justify-center rounded-full bg-gray-300'>
-				{items.length > 0 && (
+				{cartQuantity > 0 && (
 					<span className='absolute -right-2 -top-2 z-50 flex h-7 w-7 items-center justify-center rounded-full text-[10px] text-black'>
-						{items.length}
+						{cartQuantity}
 					</span>
 				)}
 				<HiOutlineShoppingBag className='headerIcon  h-8 w-8' />
