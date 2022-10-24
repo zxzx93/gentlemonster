2ebe5bffee1c858d1cb8c3a9331bbf31722b5beb
zxzx93 <thddustn26@hanmail.net>
2022-10-25 00:40:03 +0900

    [Feat] redux cartSlice에 reducer, selectors 추가
    - removeAllToCart
    - incQty
    - decQty
    - selectCartTotal
    - selectBasketItemsWithId

--- redux/cartSlice.ts
+++ redux/cartSlice.ts
@@ -18,26 +18,52 @@ export const cartSlice = createSlice({
 		addToCart: (state: CartState, action: PayloadAction<Product>) => {
 			state.items = [...state.items, action.payload];
 		},
-		removeToCart: (state: CartState, action: PayloadAction<{ id: string }>) => {
-			const idx = state.items.findIndex(
-				(item: Product) => item._id === action.payload.id,
-			);
-
+		removeAllToCart: (
+			state: CartState,
+			action: PayloadAction<{ id: string }>
+		) => {
 			const newCart = [...state.items];
-			if (idx >= 0) {
-				newCart.slice(idx, 1);
-			} else {
-				console.log(`${id}는 카트에서 삭제 할수 없습니다.`);
-			}
+			const removeAll = newCart.filter(item => item._id !== action.payload.id);
 
-			state.items = newCart;
+			state.items = removeAll;
+		},
+		incQty: (state: CartState, action: PayloadAction<{ id: string }>) => {
+			const updatedQtyItems = state.items.map(cartProduct => {
+				if (cartProduct._id === action.payload.id)
+					return {
+						...cartProduct,
+						quantity: cartProduct.quantity + 1,
+					};
+				return { ...cartProduct };
+			});
+			state.items = updatedQtyItems;
+		},
+
+		decQty: (state: CartState, action: PayloadAction<{ id: string }>) => {
+			const updatedQtyItems = state.items.map(cartProduct => {
+				if (cartProduct._id === action.payload.id)
+					return {
+						...cartProduct,
+						quantity: cartProduct.quantity - 1,
+					};
+				return { ...cartProduct };
+			});
+			state.items = updatedQtyItems;
 		},
 	},
 });
 
-export const { addToCart, removeToCart } = cartSlice.actions;
+export const { addToCart, removeAllToCart, incQty, decQty } = cartSlice.actions;
 
 // selectors
 export const selectCartItems = (state: RootState) => state.cart.items;
+export const selectCartTotal = (state: RootState) =>
+	state.cart.items.reduce(
+		(total: number, item: Product) => (total += item.price * item.quantity),
+		0
+	);
+export const selectBasketItemsWithId = (state: RootState, id: string) => {
+	state.cart.items.filter((item: Product) => item._id === id);
+};
 
 export default cartSlice.reducer;
