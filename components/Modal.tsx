6dbe7897fd311523f320188c590703db77e78bc5
yeonsu <thddustn26@hanmail.net>
2022-12-23 20:49:40 +0900

    [Add] 버튼 색상 변경
    - 결제할 상품 있을때 버튼 색상 검은색, 없을때 회색으로 변경

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -9,142 +9,142 @@ import Content from './Content';
 import { Product } from '../typings';
 
 interface ModalProps {
-	showModal: boolean;
-	setShowModal: Dispatch<SetStateAction<boolean>>;
-	items: Product[];
-	groupedItemsInCart: { [key: string]: Product[] };
-	allTotalPrice: number;
-	checkout?: {
-		createCheckout: () => Promise<void>;
-		loading: boolean;
-	};
+  showModal: boolean;
+  setShowModal: Dispatch<SetStateAction<boolean>>;
+  items: Product[];
+  groupedItemsInCart: { [key: string]: Product[] };
+  allTotalPrice: number;
+  checkout?: {
+    createCheckout: () => Promise<void>;
+    loading: boolean;
+  };
 }
 
 function Modal({
-	showModal,
-	setShowModal,
-	items,
-	groupedItemsInCart,
-	allTotalPrice,
-	checkout,
+  showModal,
+  setShowModal,
+  items,
+  groupedItemsInCart,
+  allTotalPrice,
+  checkout,
 }: ModalProps) {
-	const router = useRouter();
-	const cancelButtonRef = useRef(null);
+  const router = useRouter();
+  const cancelButtonRef = useRef(null);
 
-	return (
-		<Transition.Root show={showModal} as={Fragment}>
-			<Dialog
-				as='div'
-				className='relative z-[999]'
-				initialFocus={cancelButtonRef}
-				onClose={() => setShowModal(false)}
-			>
-				<Transition.Child
-					as={Fragment}
-					enter='ease-out duration-300'
-					enterFrom='opacity-0'
-					enterTo='opacity-100'
-					leave='ease-in duration-200'
-					leaveFrom='opacity-100'
-					leaveTo='opacity-0'
-				>
-					<div className='fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity' />
-				</Transition.Child>
+  return (
+    <Transition.Root show={showModal} as={Fragment}>
+      <Dialog
+        as='div'
+        className='relative z-[999]'
+        initialFocus={cancelButtonRef}
+        onClose={() => setShowModal(false)}
+      >
+        <Transition.Child
+          as={Fragment}
+          enter='ease-out duration-300'
+          enterFrom='opacity-0'
+          enterTo='opacity-100'
+          leave='ease-in duration-200'
+          leaveFrom='opacity-100'
+          leaveTo='opacity-0'
+        >
+          <div className='fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity' />
+        </Transition.Child>
 
-				<div className='fixed inset-0 z-[999] overflow-y-auto'>
-					<div className='flex h-full min-h-full items-end justify-end sm:items-center sm:p-0'>
-						<Transition.Child
-							as={Fragment}
-							enter='ease-out duration-300'
-							enterFrom='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
-							enterTo='opacity-100 translate-y-0 sm:scale-100'
-							leave='ease-in duration-200'
-							leaveFrom='opacity-100 translate-y-0 sm:scale-100'
-							leaveTo='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
-						>
-							<Dialog.Panel className='relative flex h-full w-[350px] max-w-sm transform flex-col overflow-hidden bg-white text-left shadow-xl transition-all sm:my-8'>
-								<div className='basis-5/6 bg-white px-5 pt-5'>
-									<div className='mt-3 text-left  sm:mt-0'>
-										<Dialog.Title
-											as='h3'
-											className='flex flex-row items-center justify-between font-title text-base font-medium leading-6 text-gray-900'
-										>
-											<p>쇼핑백</p>
-											<button type='button' onClick={() => setShowModal(false)}>
-												<IoCloseOutline className='h-5 w-5' />
-											</button>
-										</Dialog.Title>
+        <div className='fixed inset-0 z-[999] overflow-y-auto'>
+          <div className='flex h-full min-h-full items-end justify-end sm:items-center sm:p-0'>
+            <Transition.Child
+              as={Fragment}
+              enter='ease-out duration-300'
+              enterFrom='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
+              enterTo='opacity-100 translate-y-0 sm:scale-100'
+              leave='ease-in duration-200'
+              leaveFrom='opacity-100 translate-y-0 sm:scale-100'
+              leaveTo='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
+            >
+              <Dialog.Panel className='relative flex h-full w-[350px] max-w-sm transform flex-col overflow-hidden bg-white text-left shadow-xl transition-all sm:my-8'>
+                <div className='basis-5/6 bg-white px-5 pt-5'>
+                  <div className='text-left sm:mt-0'>
+                    <Dialog.Title
+                      as='h3'
+                      className='flex flex-row items-center justify-between font-title text-base font-medium leading-6 text-gray-900'
+                    >
+                      <p>쇼핑백</p>
+                      <button type='button' onClick={() => setShowModal(false)}>
+                        <IoCloseOutline className='h-5 w-5' />
+                      </button>
+                    </Dialog.Title>
 
-										<div className='no-scrollbar mt-7 h-[calc(100vh-290px)] min-h-[calc(100vh-290px)] overflow-y-auto'>
-											<div className='text-xs'>
-												{items.length > 0 ? (
-													<div className=''>
-														{Object.entries(groupedItemsInCart).map(
-															([key, contents]) => (
-																<Content
-																	key={key}
-																	id={key}
-																	contents={contents}
-																	location='cartModal'
-																/>
-															)
-														)}
-													</div>
-												) : (
-													<div className='mt-5 text-sm'>
-														<p>쇼핑백에 담긴 제품이 없습니다</p>
-													</div>
-												)}
-											</div>
-										</div>
-									</div>
-								</div>
+                    <div className='no-scrollbar mt-7 h-[calc(100vh-290px)] min-h-[calc(100vh-290px)] overflow-y-auto'>
+                      <div className='text-xs'>
+                        {items.length > 0 ? (
+                          <div>
+                            {Object.entries(groupedItemsInCart).map(
+                              ([key, contents]) => (
+                                <Content
+                                  key={key}
+                                  id={key}
+                                  contents={contents}
+                                  location='cartModal'
+                                />
+                              )
+                            )}
+                          </div>
+                        ) : (
+                          <div className='mt-5 text-sm'>
+                            <p>쇼핑백에 담긴 제품이 없습니다</p>
+                          </div>
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                </div>
 
-								<div className='flex w-full basis-1/6 flex-col px-4 py-5 font-title text-sm'>
-									<ul className='flex list-none flex-col justify-between gap-2 border-t py-6'>
-										<li className='checkoutSummary'>
-											<span>배송비</span>
-											<span>무료</span>
-										</li>
-										<li className='checkoutSummary'>
-											<span>합계</span>
-											<span>
-												<CurrencyFormat
-													value={allTotalPrice}
-													displayType='text'
-													thousandSeparator
-													suffix='원'
-												/>
-											</span>
-										</li>
-									</ul>
+                <div className='flex w-full basis-1/6 flex-col px-4 py-5 font-title text-sm'>
+                  <ul className='flex list-none flex-col justify-between gap-2 border-t py-6'>
+                    <li className='checkoutSummary'>
+                      <span>배송비</span>
+                      <span>무료</span>
+                    </li>
+                    <li className='checkoutSummary'>
+                      <span>합계</span>
+                      <span>
+                        <CurrencyFormat
+                          value={allTotalPrice}
+                          displayType='text'
+                          thousandSeparator
+                          suffix='원'
+                        />
+                      </span>
+                    </li>
+                  </ul>
 
-									<div className='flex flex-col gap-2'>
-										<Button
-											title='결제하기'
-											width='w-full'
-											height='h-[35px]'
-											buttonColor='black'
-											loading={checkout?.loading}
-											onClick={checkout?.createCheckout}
-											disabled={items.length > 0 ? false : true}
-										/>
-										<Button
-											title='자세히 보기'
-											width='w-full'
-											height='h-[35px]'
-											buttonColor='white'
-											onClick={() => router.push('/cart')}
-										/>
-									</div>
-								</div>
-							</Dialog.Panel>
-						</Transition.Child>
-					</div>
-				</div>
-			</Dialog>
-		</Transition.Root>
-	);
+                  <div className='flex flex-col gap-2'>
+                    <Button
+                      title='결제하기'
+                      width='w-full'
+                      height='h-[35px]'
+                      buttonColor={items.length ? 'black' : 'gray'}
+                      loading={checkout?.loading}
+                      onClick={checkout?.createCheckout}
+                      disabled={!(items.length > 0)}
+                    />
+                    <Button
+                      title='자세히 보기'
+                      width='w-full'
+                      height='h-[35px]'
+                      buttonColor='white'
+                      onClick={() => router.push('/cart')}
+                    />
+                  </div>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  );
 }
 
 export default Modal;
