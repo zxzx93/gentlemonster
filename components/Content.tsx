930780c0ace2c0ab7d478838d071d70635b8a627
yeonsu <thddustn26@hanmail.net>
2022-12-23 19:40:42 +0900

    [Add] 카트에 담긴 상품 이미지 클릭시 디테일 페이지로 이동

--- components/Content.tsx
+++ components/Content.tsx
@@ -1,110 +1,113 @@
-import Image from 'next/image';
 import React from 'react';
+import Image from 'next/image';
 import CurrencyFormat from 'react-currency-format';
 import toast from 'react-hot-toast';
-
 import { useDispatch } from 'react-redux';
+import Link from 'next/link';
+
 import { Product } from '../typings';
 import { urlFor } from '../sanity';
 import { removeAllToCart } from '../redux/cartSlice';
 import useQuantity from '../hooks/useQuantity';
 
 interface Props {
-	id: string;
-	contents: Product[];
-	location: 'cartModal' | 'cart';
+  id: string;
+  contents: Product[];
+  location: 'cartModal' | 'cart';
 }
 
 function Content({ id, contents, location }: Props) {
-	const dispatch = useDispatch();
-	const { itemQuantity } = useQuantity();
+  const dispatch = useDispatch();
+  const { itemQuantity } = useQuantity();
 
-	const removeAllContentFromCart = () => {
-		const removeConfirm = window.confirm('해당 상품을 삭제하시겠습니까?');
+  const removeAllContentFromCart = () => {
+    const removeConfirm = window.confirm('해당 상품을 삭제하시겠습니까?');
 
-		if (removeConfirm) {
-			dispatch(removeAllToCart({ id }));
+    if (removeConfirm) {
+      dispatch(removeAllToCart({ id }));
 
-			toast.error(`${contents[0].title}를 삭제했습니다.`, {
-				position: 'bottom-center',
-			});
-		}
-	};
+      toast.error(`${contents[0].title}를 삭제했습니다.`, {
+        position: 'bottom-center',
+      });
+    }
+  };
 
-	return (
-		<div className='flex flex-row border-b py-3.5 lg:flex-row'>
-			<div
-				className={`relative ${
-					location === 'cartModal' ? 'h-24 w-24' : 'h-44 w-44'
-				}`}
-			>
-				<Image
-					src={urlFor(contents[0].image[0]).url()}
-					layout='fill'
-					objectFit='contain'
-				/>
-			</div>
+  return (
+    <div className='flex flex-row border-b py-3.5 lg:flex-row'>
+      <Link href={`/detail/${id}`}>
+        <div
+          className={`relative cursor-pointer ${
+            location === 'cartModal' ? 'h-24 w-24' : 'h-44 w-44'
+          }`}
+        >
+          <Image
+            src={urlFor(contents[0].image[0]).url()}
+            layout='fill'
+            objectFit='contain'
+          />
+        </div>
+      </Link>
 
-			<div className='flex flex-1 items-start'>
-				<div className='relative h-full flex-1 space-y-4'>
-					<div className='justify-items  flex h-full flex-col items-start gap-x-8 text-sm'>
-						<h4 className='font-semibold'>{contents[0].title}</h4>
-						<p className='py-1'>
-							<CurrencyFormat
-								value={contents[0].price}
-								displayType='text'
-								thousandSeparator
-								suffix='원'
-							/>
-						</p>
-						<div className='pt-21 absolute bottom-0 flex gap-x-1 font-title'>
-							<div className='flex w-[80px] items-center justify-between border-[0.5px] border-zinc-600/75 px-2 py-1 text-center text-black'>
-								<button
-									type='button'
-									className='quantity'
-									onClick={() => itemQuantity('decQty', id)}
-									disabled={contents[0].quantity === 1}
-								>
-									-
-								</button>
-								<span className='quantity'>{contents[0].quantity}</span>
-								<button
-									type='button'
-									className='quantity'
-									onClick={() => itemQuantity('incQty', id)}
-									disabled={contents[0].quantity === contents[0].instock}
-								>
-									+
-								</button>
-							</div>
-						</div>
-					</div>
-				</div>
+      <div className='flex flex-1 items-start'>
+        <div className='relative h-full flex-1 space-y-4'>
+          <div className='justify-items  flex h-full flex-col items-start gap-x-8 text-sm'>
+            <h4 className='font-semibold'>{contents[0].title}</h4>
+            <p className='py-1'>
+              <CurrencyFormat
+                value={contents[0].price}
+                displayType='text'
+                thousandSeparator
+                suffix='원'
+              />
+            </p>
+            <div className='pt-21 absolute bottom-0 flex gap-x-1 font-title'>
+              <div className='flex w-[80px] items-center justify-between border-[0.5px] border-zinc-600/75 px-2 py-1 text-center text-black'>
+                <button
+                  type='button'
+                  className='quantity'
+                  onClick={() => itemQuantity('decQty', id)}
+                  disabled={contents[0].quantity === 1}
+                >
+                  -
+                </button>
+                <span className='quantity'>{contents[0].quantity}</span>
+                <button
+                  type='button'
+                  className='quantity'
+                  onClick={() => itemQuantity('incQty', id)}
+                  disabled={contents[0].quantity === contents[0].instock}
+                >
+                  +
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
 
-				<div className='flex h-full flex-col justify-between text-sm'>
-					<h4 className='fonts-semiblod'>
-						<CurrencyFormat
-							value={contents.reduce(
-								(total, item) => (total += item.price * item.quantity),
-								0
-							)}
-							displayType='text'
-							thousandSeparator
-							suffix='원'
-						/>
-					</h4>
+        <div className='flex h-full flex-col justify-between text-sm'>
+          <h4 className='fonts-semiblod'>
+            <CurrencyFormat
+              value={contents.reduce(
+                (total, item) => (total += item.price * item.quantity),
+                0
+              )}
+              displayType='text'
+              thousandSeparator
+              suffix='원'
+            />
+          </h4>
 
-					<button
-						type='button'
-						onClick={removeAllContentFromCart}
-						className='box-border self-end border-b border-black text-black'
-					>
-						삭제
-					</button>
-				</div>
-			</div>
-		</div>
-	);
+          <button
+            type='button'
+            onClick={removeAllContentFromCart}
+            className='box-border self-end border-b border-black text-black'
+          >
+            삭제
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Content;
--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
-import { useRouter } from 'next/router';
 import { useSelector } from 'react-redux';
 import CurrencyFormat from 'react-currency-format';
 
@@ -12,111 +11,110 @@ import Content from '../../components/Content';
 import useCreateCheckout from '../../hooks/useCreateCheckout';
 
 function Cart() {
-	const items = useSelector(selectCartItems);
-	const allTotal = useSelector(selectCartTotal);
-	const router = useRouter();
-	const { createCheckout, loading } = useCreateCheckout();
+  const items = useSelector(selectCartItems);
+  const allTotal = useSelector(selectCartTotal);
+  const { createCheckout, loading } = useCreateCheckout();
 
-	const [groupedItemsInCart, setGroupedItemsInCart] = useState(
-		{} as { [key: string]: Product[] }
-	);
+  const [groupedItemsInCart, setGroupedItemsInCart] = useState(
+    {} as { [key: string]: Product[] }
+  );
 
-	useEffect(() => {
-		const groupedItems = items.reduce((results, item) => {
-			(results[item._id] = results[item._id] || []).push(item);
-			return results;
-		}, {} as { [key: string]: Product[] }); // 초기엔 빈 객체, 타입 정의
+  useEffect(() => {
+    const groupedItems = items.reduce((results, item) => {
+      (results[item._id] = results[item._id] || []).push(item);
+      return results;
+    }, {} as { [key: string]: Product[] }); // 초기엔 빈 객체, 타입 정의
 
-		setGroupedItemsInCart(groupedItems);
-	}, [items]);
+    setGroupedItemsInCart(groupedItems);
+  }, [items]);
 
-	return (
-		<div>
-			<Head>
-				<title>Gentlemonster</title>
-				<link rel='icon' href='/favicon.ico' />
-			</Head>
-			<Header />
+  return (
+    <div>
+      <Head>
+        <title>Gentlemonster</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <Header />
 
-			<main className='flex max-w-[1419px] flex-col gap-8 px-4 pt-5 font-title md:flex-row md:px-12 md:pb-12 md:pt-[150px] lg:gap-20 xl:mx-auto xl:my-0'>
-				{/* 쇼핑백 */}
-				<div className='basis-2/3'>
-					<h1 className='mb-10 text-base font-semibold text-black'>쇼핑백</h1>
+      <main className='flex max-w-[1419px] flex-col gap-8 px-4 pt-5 font-title md:flex-row md:px-12 md:pb-12 md:pt-[150px] lg:gap-20 xl:mx-auto xl:my-0'>
+        {/* 쇼핑백 */}
+        <div className='basis-2/3'>
+          <h1 className='mb-10 text-base font-semibold text-black'>쇼핑백</h1>
 
-					<div className='flex flex-row justify-between border-b pb-6 text-[13px] font-normal'>
-						<p>상품</p>
-						<p>가격</p>
-					</div>
+          <div className='flex flex-row justify-between border-b pb-6 text-[13px] font-normal'>
+            <p>상품</p>
+            <p>가격</p>
+          </div>
 
-					{items.length > 0 ? (
-						<div>
-							{Object.entries(groupedItemsInCart).map(([key, contents]) => (
-								<Content
-									key={key}
-									id={key}
-									contents={contents}
-									location='cart'
-								/>
-							))}
-						</div>
-					) : (
-						<div className='mt-5 text-sm'>쇼핑백에 담긴 제품이 없습니다</div>
-					)}
-				</div>
+          {items.length > 0 ? (
+            <div>
+              {Object.entries(groupedItemsInCart).map(([key, contents]) => (
+                <Content
+                  key={key}
+                  id={key}
+                  contents={contents}
+                  location='cart'
+                />
+              ))}
+            </div>
+          ) : (
+            <div className='mt-5 text-sm'>쇼핑백에 담긴 제품이 없습니다</div>
+          )}
+        </div>
 
-				{/* 주문상세 */}
-				<div className='mt-[73px] basis-1/3 text-sm md:sticky md:top-[150px] md:mt-0 md:h-80'>
-					<h1 className='mb-5 text-base font-semibold text-black'>주문상세</h1>
+        {/* 주문상세 */}
+        <div className='mt-[73px] basis-1/3 text-sm md:sticky md:top-[150px] md:mt-0 md:h-80'>
+          <h1 className='mb-5 text-base font-semibold text-black'>주문상세</h1>
 
-					<ul className='flex list-none flex-col justify-between border-t pt-4'>
-						<li className='checkoutSummary'>
-							<span>소계</span>
-							<span>
-								<CurrencyFormat
-									value={allTotal}
-									displayType='text'
-									thousandSeparator
-									suffix='원'
-								/>
-							</span>
-						</li>
-						<li className='checkoutSummary mt-2'>
-							<span>배송비</span>
-							<span>무료</span>
-						</li>
-						<li className='checkoutSummary mt-4 border-t pt-5'>
-							<span>합계</span>
-							<span>
-								<CurrencyFormat
-									value={allTotal}
-									displayType='text'
-									thousandSeparator
-									suffix='원'
-								/>
-							</span>
-						</li>
-					</ul>
+          <ul className='flex list-none flex-col justify-between border-t pt-4'>
+            <li className='checkoutSummary'>
+              <span>소계</span>
+              <span>
+                <CurrencyFormat
+                  value={allTotal}
+                  displayType='text'
+                  thousandSeparator
+                  suffix='원'
+                />
+              </span>
+            </li>
+            <li className='checkoutSummary mt-2'>
+              <span>배송비</span>
+              <span>무료</span>
+            </li>
+            <li className='checkoutSummary mt-4 border-t pt-5'>
+              <span>합계</span>
+              <span>
+                <CurrencyFormat
+                  value={allTotal}
+                  displayType='text'
+                  thousandSeparator
+                  suffix='원'
+                />
+              </span>
+            </li>
+          </ul>
 
-					<div className='mt-10'>
-						<Button
-							title='결제하기'
-							buttonColor='black'
-							width='w-full'
-							height='h-10'
-							loading={loading}
-							onClick={createCheckout}
-							disabled={!(items.length > 0)}
-						/>
-					</div>
+          <div className='mt-10'>
+            <Button
+              title='결제하기'
+              buttonColor='black'
+              width='w-full'
+              height='h-10'
+              loading={loading}
+              onClick={createCheckout}
+              disabled={!(items.length > 0)}
+            />
+          </div>
 
-					<p className='my-4 mt-6 text-xs leading-6 tracking-tighter'>
-						무료 반품 서비스가 제공되오니 안심하고 구매하십시오. 배송에 대한
-						자세한 내용을 확인하십시오
-					</p>
-				</div>
-			</main>
-		</div>
-	);
+          <p className='my-4 mt-6 text-xs leading-6 tracking-tighter'>
+            무료 반품 서비스가 제공되오니 안심하고 구매하십시오. 배송에 대한
+            자세한 내용을 확인하십시오
+          </p>
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default Cart;
